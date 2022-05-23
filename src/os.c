442474861feeeef4e3a5526bddf69cc9666c0f96
Niki Hansche <mf-developing@hansche.de>
2022-05-23 17:18:01 +0200

    Add ip6tables rules for forwarding/masqerading

--- src/os.c
+++ src/os.c
@@ -455,6 +455,10 @@ Cmds firewall_rules_cmds(int is_server)
                   "iptables -t filter -A FORWARD -i $EXT_IF_NAME -o $IF_NAME -m state --state "
                   "RELATED,ESTABLISHED -j ACCEPT",
                   "iptables -t filter -A FORWARD -i $IF_NAME -o $EXT_IF_NAME -j ACCEPT",
+                  "ip6tables -t nat -A POSTROUTING -o $EXT_IF_NAME -s $REMOTE_TUN_IP6 -j MASQUERADE",
+                  "ip6tables -t filter -A FORWARD -i $EXT_IF_NAME -o $IF_NAME -m state --state "
+                  "RELATED,ESTABLISHED -j ACCEPT",
+                  "ip6tables -t filter -A FORWARD -i $IF_NAME -o $EXT_IF_NAME -j ACCEPT",
                   NULL },
             *unset_cmds[] = {
                 "iptables -t nat -D POSTROUTING -o $EXT_IF_NAME -s $REMOTE_TUN_IP -j MASQUERADE",
@@ -463,6 +467,10 @@ Cmds firewall_rules_cmds(int is_server)
                 "iptables -t filter -D FORWARD -i $IF_NAME -o $EXT_IF_NAME -j ACCEPT",
                 "iptables -t raw -D PREROUTING ! -i $IF_NAME -d $LOCAL_TUN_IP -m addrtype ! "
                 "--src-type LOCAL -j DROP",
+                "ip6tables -t nat -D POSTROUTING -o $EXT_IF_NAME -s $REMOTE_TUN_IP6 -j MASQUERADE",
+                "ip6tables -t filter -D FORWARD -i $EXT_IF_NAME -o $IF_NAME -m state --state "
+                "RELATED,ESTABLISHED -j ACCEPT",
+                "ip6tables -t filter -D FORWARD -i $IF_NAME -o $EXT_IF_NAME -j ACCEPT",
                 NULL
             };
 #elif defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || \
